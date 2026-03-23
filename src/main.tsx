31875f2cbd709716d8ff8fc273931d6f952a410a
ph2oconsult-pixeldiffboho <ph2oconsult@gmail.com>
2026-03-23 11:23:33 +1100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import AppMain from './AppMain.tsx';
 import './index.css';
 import './i18n/config';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppMain />
   </StrictMode>,
 );
